8458a4930b8f073a92a1060dba29ebcfc83654c3
Uliana Khmeleva <ulechka.hmeleva@gmail.com>
2023-07-12 14:34:23 +0700

    Initial Commit

---
+++ FontBlaster/FontBlaster.h
@@ -0,0 +1,18 @@
+//
+//  FontBlaster.h
+//  FontBlaster
+//
+//  Created by Ulechka Khmeleva on 12.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FontBlaster.
+FOUNDATION_EXPORT double FontBlasterVersionNumber;
+
+//! Project version string for FontBlaster.
+FOUNDATION_EXPORT const unsigned char FontBlasterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FontBlaster/PublicHeader.h>
+
+
